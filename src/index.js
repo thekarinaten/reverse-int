fa5c5cabd8ba997270437af3770c3496275e5574
Karina Ten <karina.ten22@gmail.com>
2023-02-27 23:01:47 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+ 
+  let val = n.toString().split('').reverse().join('');
+    
+  if (val.endsWith('-')) {
+    val = '' + val;
+      return parseInt(val);
+  }
+      return parseInt(val);
 }
+
